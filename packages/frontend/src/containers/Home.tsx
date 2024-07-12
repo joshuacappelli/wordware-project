e25d7f08e3e7d0d32c97324720723dae9771fd48
joshuacappelli <joshua@cappelliconnect.com>
2024-07-11 21:05:40 -0400

    set up react app

---
+++ packages/frontend/src/containers/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Tinker</h1>
+        <p className="text-muted">A simple prompt app for building AI bots through hugging face</p>
+      </div>
+    </div>
+  );
+}
